709dfd90dd0762e10d4b54d2679e6e5a5d81873b
RulerDevansh <srivastavadevansh123@gmail.com>
2025-08-02 07:56:07 +0530

    vercel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,26 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
   
-  server: {
+  server : {
     proxy: {
       '/api': 'http://localhost:8000',
     },
   },
-  
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: './index.html'
-      }
-    }
-  },
-  
-  // Ensure proper handling of client-side routing
-  preview: {
-    port: 5173,
-    strictPort: true,
-  }
+  plugins: [react()],
+
 })
